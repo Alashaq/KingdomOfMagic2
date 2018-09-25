77f6b8128f9ab35756861e12dce216a3d92b5b62
Alashaq <40842773+Alashaq@users.noreply.github.com>
2018-09-26 00:03:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(سباام يولد يلعن اومم الفله ${x})
+        message.channel.send(`سبام يولد ينعل ام الفلة hello Again ${x}`)
           .then(m => {
             count++;
           })
